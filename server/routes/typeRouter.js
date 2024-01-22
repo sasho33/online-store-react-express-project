e9b11e24954f80a07c7a0fdbfb315cd7c8e291c6
sasho33 <sasho3333333@gmail.com>
2024-01-22 13:32:22 +0000

    react component and client structure

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express'); // Import express Router
 const router = new Router(); // Create Router object
 const typeController = require('../controllers/typeController'); // Import typeController object
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
-router.post('/', typeController.create); // Add post request handler for route '/type'
+router.post('/', checkRoleMiddleware('ADMIN'), typeController.create); // Add post request handler for route '/type'
 router.get('/', typeController.getAll); // Add get request handler for route '/type'
 
 module.exports = router; // Export router object
